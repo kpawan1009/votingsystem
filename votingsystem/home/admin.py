1de78d76ad6628075fba65d39a7440bc60e48e2a
kpawan1009 <103193620+kpawan1009@users.noreply.github.com>
2022-04-28 18:21:46 +0530

    Add files via upload

---
+++ votingsystem/home/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Candidatepalg),
+
+admin.site.register(models.Candidateaura),
+
+admin.site.register(models.Votergovt),
+
+admin.site.register(models.Voterregisteredpalghar),
+
+admin.site.register(models.Voterregisteredamroha),
+
+admin.site.register(models.UIDamroha),
+
+admin.site.register(models.UIDpalghar),
+
+admin.site.register(models.Feedback),
